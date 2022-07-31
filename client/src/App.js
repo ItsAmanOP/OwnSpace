2209776c5f695b0b3b97c9b01c289e4ddc1329e3
ItsAmanOP <aman.karpentarcs2018@indoreinstitute.com>
2022-07-31 17:06:19 +0530

    Generating random id for each docs

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,17 @@ import React from "react";
 import './App.css';
 import Editor from "./component/Editor";
 
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { v4 as uuid } from "uuid";
+
 function App() {
   return (
-    <>
-      <Editor />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Navigate replace to={`/docs/${uuid()}`}/> } />
+        <Route path='/docs/:id' element={<Editor />} />
+      </Routes>
+    </Router>
   );
 }
 
