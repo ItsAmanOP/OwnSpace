4b7c8b43f3ca09b7c99b44759e8d80844cce34bd
ItsAmanOP <aman.karpentarcs2018@indoreinstitute.com>
2022-08-01 00:54:11 +0530

    Implemented controller in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import { Server } from "socket.io";
+import Connection from "./database/db.js";
+import { getDocument, updateDocument } from "./controller/document-controller.js";
 
 const PORT = 9000;
+
+Connection();
 const io = new Server(PORT, {
     cors: {
         origin: 'http://localhost:3000',
@@ -9,13 +13,18 @@ const io = new Server(PORT, {
 });
 
 io.on('connection', socket => {
-    socket.on('get-document', documentId => {
-        const data = "";
+    socket.on('get-document', async documentId => {
+        const document = await getDocument(documentId);
         socket.join(documentId);
-        socket.emit('load-document', data);
+        socket.emit('load-document', document.data);
 
-        socket.on('send-changes', delta => {                                  // Catching changes in socket sent by Quill from the frontend
-            socket.broadcast.to(documentId).emit('receive-changes', delta);                  // Broadcasting the backend changes to all the frontend users with same Id
+        socket.on('send-changes', delta => {                               // Catching changes in socket sent by Quill from the frontend.
+            socket.broadcast.to(documentId).emit('receive-changes', delta); // Broadcasting the backend changes to all the frontend 
+                                                                            // users with same Id.
+        })
+
+        socket.on('save-document', async data => {
+            await updateDocument(documentId, data);
         })
     })
     
