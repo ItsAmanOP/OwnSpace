20df9da18df527e19c5dccf2caae988916c59cf4
ItsAmanOP <aman.karpentarcs2018@indoreinstitute.com>
2022-08-04 21:44:05 +0530

    APIs for custom react components

---
+++ client/src/API/serviceApi.js
@@ -0,0 +1,40 @@
+const serviceapi = [
+  {
+    id: 1,
+    logo: "fas fa-download",
+    title: "Register for free.",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 2,
+    logo: "fas fa-user-check",
+    title: "Verify your identity ",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 3,
+    logo: "fas fa-money-check-alt",
+    title: "Get your OwnSpace premium",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 4,
+    logo: "fab fa-amazon-pay",
+    title: "Pay for your sub!",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 5,
+    logo: "fas fa-hand-holding-usd",
+    title: "Get value from your time",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 6,
+    logo: "fas fa-globe",
+    title: "That's it.",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+];
+
+export default serviceapi;
---
+++ client/src/API/workApi.js
@@ -0,0 +1,22 @@
+const workapi = [
+  {
+    id: 1,
+    logo: "fas fa-download",
+    title: "Sign Up",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 2,
+    logo: "fas fa-chalkboard-teacher",
+    title: "Start Wrorking",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 3,
+    logo: "fas fa-donate",
+    title: "Get Premium For More",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+];
+
+export default workapi;
